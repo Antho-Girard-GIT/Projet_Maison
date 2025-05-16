ff1e33cc89f6b6f7ac5fa836edf15c38fa5e373e
Anthonny Girard <anthonnygirard@gmail.com>
2025-05-16 13:03:55 -0400

    five commit

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,11 +1,13 @@
 import { Navbar } from "./page/navbar";
 import { Outlet } from "react-router-dom";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function Layout() {
   return (
     <>
       <Navbar />
       <Outlet />
+      <SpeedInsights />
     </>
   );
 }
